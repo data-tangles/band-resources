b9530623e0122a2b26b77f79dbf3aa5eb01e3cdc
Cuan Leo <cuanleo@gmail.com>
2025-03-09 17:52:51 +0200

    fix: Remove incorrect resource reference from DigitalOcean project configuration

--- terraform/project.tf
+++ terraform/project.tf
@@ -4,8 +4,7 @@ resource "digitalocean_project" "band-resources" {
   purpose     = var.digitalocean_project_purpose
   environment = var.digitalocean_project_environment
   resources = [
-    digitalocen_droplet.docker.urn,
-    digitalocean_vpc.docker_vpc.urn,
-    digitalocean_firewall.firewall.urn
+    digitalocean_droplet.docker.urn,
+    digitalocean_vpc.docker_vpc.urn
   ]
 }
