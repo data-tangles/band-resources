bb4bcf3555b816aa2d03a2b0fb178927d43d2f47
Cuan Leo <cuanleo@gmail.com>
2025-03-09 17:46:39 +0200

    fix: Update Terraform workflow and project configuration to remove formatting check and include additional resources in the project

--- terraform/project.tf
+++ terraform/project.tf
@@ -4,6 +4,8 @@ resource "digitalocean_project" "band-resources" {
   purpose     = var.digitalocean_project_purpose
   environment = var.digitalocean_project_environment
   resources = [
-    digitalocean_droplet.foobar.urn
+    digitalocen_droplet.docker.id,
+    digitalocean_vpc.docker_vpc.id,
+    digitalocean_firewall.docker_firewall.id
   ]
 }
