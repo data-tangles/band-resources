91aad9a258ad6a781648e6ffdd378e7b72e3d996
Cuan Leo <cuanleo@gmail.com>
2025-03-21 19:57:46 +0200

    feat: Add Docker SSH key resource for DigitalOcean droplet

--- terraform/digital_ocean/droplet.tf
+++ terraform/digital_ocean/droplet.tf
@@ -1,3 +1,8 @@
+resource "digitalocean_ssh_key" "docker" {
+  name       = "Docker SSH Key"
+  public_key = file("./ssh_keys/docker_ssh_key.pub")
+}
+
 resource "digitalocean_droplet" "docker" {
   image    = var.digitalocean_droplet_image
   name     = var.digitalocean_droplet_name
@@ -6,6 +11,7 @@ resource "digitalocean_droplet" "docker" {
   backups  = var.digitalocean_droplet_backups
   vpc_uuid = digitalocean_vpc.docker_vpc.id
   tags     = var.tags
+  ssh_keys = [digitalocean_ssh_key.docker.fingerprint]
 
   dynamic "backup_policy" {
     for_each = var.digitalocean_droplet_backups ? [1] : []
